0058ed95ae0dd44f3d981dd143afc4d47fff2fd5
carlosSalem <budaspow@gmail.com>
2018-07-11 12:55:39 +0200

    UPDATE
    UPDATE

--- app/Http/Controllers/evolucionController.php
+++ app/Http/Controllers/evolucionController.php
@@ -6,5 +6,7 @@ use Illuminate\Http\Request;
 
 class evolucionController extends Controller
 {
-    //
+    public function inicio(){
+        return view ("evolucion");
+    }
 }
--- app/Http/Controllers/examenesController.php
+++ app/Http/Controllers/examenesController.php
@@ -6,5 +6,7 @@ use Illuminate\Http\Request;
 
 class examenesController extends Controller
 {
-    //
+    public function inicio(){
+        return view ("examenes");
+    }
 }
